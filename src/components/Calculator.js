37250f7febce950d9eafcdbc5034dd4cd30ba524
david-lafontant <lafontant.david@gmail.com>
2022-03-01 14:46:23 -0600

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -18,54 +18,51 @@ const Calculator = () => {
   };
 
   return (
-<div className='pageContainer'>
-<p className="description">Let&apos;s do some math!</p>
-<div className="container">
-      <div className="row screen ">
-        { properties.total}
-        { properties.operation }
-        { properties.next }
-      </div>
-      <div className="row ">
+    <div className="pageContainer">
 
-        <button type="button" onClick={handleClick} className="item">AC</button>
-        <button type="button" onClick={handleClick} className="item">+/-</button>
-        <button type="button" onClick={handleClick} className="item">%</button>
-        <button type="button" onClick={handleClick} className="item last-col">÷</button>
+      <div className="hail">
+        <p>Let&apos;s do some math!</p>
       </div>
-      <div className="row ">
-        <button type="button" onClick={handleClick} className="item">7</button>
-        <button type="button" onClick={handleClick} className="item">8</button>
-        <button type="button" onClick={handleClick} className="item">9</button>
-        <button type="button" onClick={handleClick} className="item last-col">x</button>
+      <div className="container">
+        <div className="row screen ">
+          {properties.total}
+          {properties.operation}
+          {properties.next}
+        </div>
+        <div className="row ">
+
+          <button type="button" onClick={handleClick} className="item">AC</button>
+          <button type="button" onClick={handleClick} className="item">+/-</button>
+          <button type="button" onClick={handleClick} className="item">%</button>
+          <button type="button" onClick={handleClick} className="item last-col">÷</button>
+        </div>
+        <div className="row ">
+          <button type="button" onClick={handleClick} className="item">7</button>
+          <button type="button" onClick={handleClick} className="item">8</button>
+          <button type="button" onClick={handleClick} className="item">9</button>
+          <button type="button" onClick={handleClick} className="item last-col">x</button>
+        </div>
+
+        <div className="row ">
+          <button type="button" onClick={handleClick} className="item">4</button>
+          <button type="button" onClick={handleClick} className="item">5</button>
+          <button type="button" onClick={handleClick} className="item">6</button>
+          <button type="button" onClick={handleClick} className="item last-col">-</button>
+        </div>
+        <div className="row ">
+          <button type="button" onClick={handleClick} className="item">1</button>
+          <button type="button" onClick={handleClick} className="item">2</button>
+          <button type="button" onClick={handleClick} className="item">3</button>
+          <button type="button" onClick={handleClick} className="item last-col">+</button>
+        </div>
+        <div className="row last row">
+          <button type="button" onClick={handleClick} className="item zero">0</button>
+          <button type="button" onClick={handleClick} className="item">.</button>
+          <button type="button" onClick={handleClick} className="item last-col">=</button>
+        </div>
       </div>
 
-      <div className="row ">
-        <button type="button" onClick={handleClick} className="item">4</button>
-        <button type="button" onClick={handleClick} className="item">5</button>
-        <button type="button" onClick={handleClick} className="item">6</button>
-        <button type="button" onClick={handleClick} className="item last-col">-</button>
-      </div>
-      <div className="row ">
-        <button type="button" onClick={handleClick} className="item">1</button>
-        <button type="button" onClick={handleClick} className="item">2</button>
-        <button type="button" onClick={handleClick} className="item">3</button>
-        <button type="button" onClick={handleClick} className="item last-col">+</button>
-      </div>
-      <div className="row last row">
-        <button type="button" onClick={handleClick} className="item zero">0</button>
-        <button type="button" onClick={handleClick} className="item">.</button>
-        <button type="button" onClick={handleClick} className="item last-col">=</button>
-      </div>
     </div>
-
-
-
-</div>
-
-
-
-
 
   );
 };
